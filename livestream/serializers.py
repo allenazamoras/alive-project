2deb9d87c6d39eade226cfd30725b951f13aacfc
Ephraime Godinez <cs.exlgodinez@gmail.com>
2018-05-14 17:02:54 +0800

    Modify User and Session serializers

--- livestream/serializers.py
+++ livestream/serializers.py
@@ -4,16 +4,18 @@ from .models import Session, User, ClientToken
 # SERIALIZERS HERE
 
 
-class SessionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Session
-        fields = ('id', 'session_name', 'session_id')
-
-
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'first_name', 'last_name')
+
+
+class SessionSerializer(serializers.HyperlinkedModelSerializer):
+    owner = UserSerializer()
+
+    class Meta:
+        model = Session
+        fields = ('id', 'session_name', 'session_id', 'owner')
 
 
 class ClientTokenSerializer(serializers.ModelSerializer):
