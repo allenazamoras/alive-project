addd7ebe207e9495fd4baedb09a96e312342c326
Ephraime Godinez <cs.exlgodinez@gmail.com>
2018-05-15 09:59:11 +0800

    Modify index url to make API Root accessible

--- livestream/urls.py
+++ livestream/urls.py
@@ -11,7 +11,7 @@ router.register(r'user', views.UserViewSet, base_name='user')
 router.register(r'token', views.ClientTokenViewSet, base_name='token')
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('index/', views.IndexView.as_view(), name='index'),
     # It worked I have no idea how ref: livestream/index.html
     path('session/<int:pk>', views.SessionDetailView.as_view(), name='stream'),
 ]
