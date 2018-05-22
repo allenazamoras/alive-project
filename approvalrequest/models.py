a254233135dc0b5b86cb782bbf5ffd3a77c48c19
Ephraime Godinez <cs.exlgodinez@gmail.com>
2018-05-22 09:55:22 +0800

    create ApprovalRequest model

--- approvalrequest/models.py
+++ approvalrequest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from livestream.models import Appeal
+from userprofile.models import User
 
-# Create your models here.
+
+class ApprovalRequest(models.Model):
+    appeal = models.ForeignKey(Appeal, on_delete=models.CASCADE,
+                               related_name='approval_requests')
+    helper = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_approved = models.NullBooleanField()
+
+    def __str__(self):
+        return self.helper + "wants to be helper for"
+        + self.appeal.request_title
