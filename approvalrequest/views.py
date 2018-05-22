9d3f5da4b9add9378d6ac0ffc9988fd701d6b13f
Ephraime Godinez <cs.exlgodinez@gmail.com>
2018-05-22 14:45:16 +0800

    modify create and list method

--- approvalrequest/views.py
+++ approvalrequest/views.py
@@ -11,7 +11,21 @@ class ApprovalRequestViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         # when user presses "HELP" button
-        # an ApprovalRequest instance gets created
+        # an ApprovalRequest instance gets created,
+        # UNLESS it already exists
+        appeal_instance = self.request.data['appeal']
+        if ApprovalRequest.objects.filter(
+            appeal=appeal_instance,
+                helper=self.request.user).exists():
+            # WARNING: if request gets rejected (is_accepted holds false)
+            # return message will still be 'pending approval...'
+            return Response({'return': 'pending approval...'})
+
+        # owner should NOT BE ABLE TO create approvalrequess
+        # for their OWN appeals, that's stupid
+        if appeal_instance.owner == self.request.user:
+            return Response({'return': "action impossible"})
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -20,9 +34,11 @@ class ApprovalRequestViewSet(viewsets.ModelViewSet):
                         headers=headers)
 
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+        # display list of all ApprovalRequests
+        # should only display ApprovalRequests that have
+        # not been rejected (is_accepted holds null)
+        queryset = ApprovalRequest.objects.exclude(is_accepted=False)
 
-        # display list of all ApprovalRequests for that appeal
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
@@ -32,13 +48,20 @@ class ApprovalRequestViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def partial_update(self, request, *args, **kwargs):
-        # when a request gets accepted other instances
-        # from the same appeal should be deleted
         kwargs['partial'] = True
         return self.update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         # when approval request gets rejected it gets deleted from the db
+        # when user revokes approval request it gets deleted from the db
+        # when a request gets accepted other instances
+        # from the same appeal should be deleted
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        return Response(serializer.data)
