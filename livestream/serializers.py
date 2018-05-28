0e3beac9509bcb89e447dc7fc8ca3f751bcb34d7
Ephraime Godinez <cs.exlgodinez@gmail.com>
2018-05-28 15:09:54 +0800

    modified to send appropriate data

--- livestream/serializers.py
+++ livestream/serializers.py
@@ -3,22 +3,19 @@ from livestream.models import Appeal, ApprovalRequest
 from userprofile.models import User
 
 
-class OwnerSerializer(serializers.ModelSerializer):
+class BaseUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username',
                   'first_name', 'last_name',
-                  'profile_picture')
+                  'profile_picture',)
 
 
-class HelperSerializer(serializers.ModelSerializer):
+class HelperSerializer(BaseUserSerializer):
     reputation = serializers.SerializerMethodField()
 
-    class Meta:
-        model = User
-        fields = ('username',
-                  'first_name', 'last_name',
-                  'profile_picture', 'reputation')
+    class Meta(BaseUserSerializer.Meta):
+        fields = BaseUserSerializer.Meta.fields + ('reputation',)
 
     def get_reputation(self, obj):
         # TO DO when user rating is up
@@ -33,16 +30,16 @@ class PendingListSerializer(serializers.ModelSerializer):
 
 class AppealSerializer(serializers.ModelSerializer):
     # nested serialization see drf docu for more info
-    owner = OwnerSerializer()
+    owner = BaseUserSerializer()
     helper = HelperSerializer()
     pending_list = serializers.SerializerMethodField()
     token = serializers.SerializerMethodField()
 
     class Meta:
         model = Appeal
-        fields = ('id', 'request_title', 'session_id', 'detail',
+        fields = ('id', 'request_title', 'detail',
                   'date_pub', 'owner', 'helper', 'is_active',
-                  'pending_list', 'token')
+                  'pending_list', 'session_id', 'token')
 
     def get_pending_list(self, obj):
         plist = PendingListSerializer(
@@ -53,20 +50,8 @@ class AppealSerializer(serializers.ModelSerializer):
         return self.context.get('token', '')
 
 
-class OpenAppealSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Appeal
-        fields = ('request_title', 'detail', 'date_pub')
-
-
-class ClosedAppealSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Appeal
-        fields = ('request_title', 'detail')
-
-
 class AppealSerializerForHelpers(serializers.ModelSerializer):
-    owner = OwnerSerializer()
+    owner = BaseUserSerializer()
 
     class Meta:
         model = Appeal
