d65c194544f30a37b51ad5fbd0fe8de02b680760
Ephraime Godinez <cs.exlgodinez@gmail.com>
2018-05-28 11:50:50 +0800

    modified serializers to send relevant data

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from userprofile.models import User
-from livestream.models import Appeal
-from livestream.serializers import OpenAppealSerializer
-from livestream.serializers import ClosedAppealSerializer
+from livestream.models import Appeal, ApprovalRequest
+
+
+class BaseAppealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appeal
+        fields = ('request_title', 'detail',)
+
+
+class OpenAppealSerializer(BaseAppealSerializer):
+    class Meta:
+        model = Appeal
+        fields = BaseAppealSerializer.Meta.fields + ('date_pub',)
+
+
+class OfferSerializer(serializers.ModelSerializer):
+    appeal = OpenAppealSerializer()
+
+    class Meta:
+        model = ApprovalRequest
+        fields = ('appeal', 'is_approved')
+
 
 class UserSerializer(serializers.ModelSerializer):
     offers = serializers.SerializerMethodField()
@@ -26,5 +45,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     def get_closedappeals(self, obj):
         appeals = Appeal.objects.filter(owner=obj, is_active=False)
-        serializer = ClosedAppealSerializer(appeals, many=True)
+        serializer = BaseAppealSerializer(appeals, many=True)
         return serializer.data
