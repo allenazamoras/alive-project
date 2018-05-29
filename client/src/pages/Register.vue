891fe1593b15f99f279cc8b1536ea3ccf54e7c52
Michael <michaelgao14@gmail.com>
2018-05-29 13:33:50 +0800

    fixed formatting and removed the extra register button

--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -4,59 +4,52 @@
             <v-layout align-center justify-center> 
                 <v-flex xs10 sm8 md7 lg4 xl3>
                     <v-card class="pa-4 elevation-10">
-                        <!-- <v-card-text class="text-xs-center">
-                        
-                        </v-card-text> -->
-                        
                         <v-card-text class="subheading text-xs-center grey--text text--darken-2" id="greeting" style="opacity: 0;">
                             We're glad you're here.
                         </v-card-text>
                         <v-form class="pa-4 pb-5">
                             <v-text-field
-                      label="Username"
-                      v-model="username"
-                  ></v-text-field>
-                  <v-text-field
-                      label="Password"
-                      v-model="password"
-                      :append-icon="seePass ? 'visibility_off' : 'visibility'"
-                      :append-icon-cb="() => (seePass = !seePass)"
-                      :type="seePass ? 'text' : 'password'"
-                  
-                  ></v-text-field>
+                                label="Username"
+                                v-model="username"
+                            ></v-text-field>
+                            <v-text-field
+                                label="Password"
+                                v-model="password"
+                                :append-icon="seePass ? 'visibility_off' : 'visibility'"
+                                :append-icon-cb="() => (seePass = !seePass)"
+                                :type="seePass ? 'text' : 'password'"
+                            
+                            ></v-text-field>
 
-                  <v-layout>
-                    <v-flex xs12 sm12 md6>
-                      <v-text-field
-                          label="First Name"
-                          v-model="first_name"
-                      ></v-text-field>
-                    </v-flex>
+                            <v-layout>
+                                <v-flex xs12 sm12 md6>
+                                <v-text-field
+                                    label="First Name"
+                                    v-model="first_name"
+                                ></v-text-field>
+                                </v-flex>
 
-                    <v-flex xs12 sm12 md6>
-                      <v-text-field
-                          label="Last Name"
-                          v-model="last_name"
-                      ></v-text-field>
-                    </v-flex>
-                    
-                  </v-layout>
-                  <v-select
-                    v-model="gender"
-                    :items="genderList"
-                    label="Gender"
-                    single-line>
-                  </v-select>
-                  <v-btn color="success" @click="register()">Register</v-btn>
+                                <v-flex xs12 sm12 md6>
+                                <v-text-field
+                                    label="Last Name"
+                                    v-model="last_name"
+                                ></v-text-field>
+                                </v-flex>
+                                
+                            </v-layout>
+                            <v-select
+                                v-model="gender"
+                                :items="genderList"
+                                label="Gender"
+                                single-line>
+                            </v-select>
                         </v-form>
                         <v-card-text flex>
                                 <v-btn color="primary" @click="register" large>Register</v-btn>
                                 <router-link to="/login">Sign in instead</router-link>
                         </v-card-text>
-                        
                     </v-card>
                 </v-flex>
-
                 <snackbar :text="snackbarTitle"/>         
             </v-layout>
         </v-container>
