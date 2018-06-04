83218e7b4f775990299533cdddd73abda8f57369
Ephraime Godinez <cs.exlgodinez@gmail.com>
2018-06-04 16:22:48 +0800

    initial commit

---
+++ aLive/routing.py
@@ -0,0 +1,8 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path, include
+
+
+application = ProtocolTypeRouter({
+	# TODO
+})
