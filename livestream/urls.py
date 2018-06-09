a343edcf80b4b684d6783df1fb4a082d5a9ebdba
allenazamoras <allenazamoras@gmail.com>
2018-06-09 20:09:17 +0800

    Edit category

--- livestream/urls.py
+++ livestream/urls.py
@@ -8,6 +8,7 @@ app_name = 'livestream'
 router = routers.DefaultRouter()
 router.register(r'request', views.AppealViewSet, base_name='request')
 router.register(r'pending', views.ApprovalRequestViewSet, base_name='pending')
+router.register(r'category', views.CategoryViewSet, base_name='category')
 
 urlpatterns = [
 ]
